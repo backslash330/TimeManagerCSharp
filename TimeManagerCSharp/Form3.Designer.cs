352fe9f873433f4c6bc72c07eff28b681b874035
Nicholas Almeida <n.almeida330@gmail.com>
2021-08-07 15:54:05 -0600

    began work on support ticket system

--- TimeManagerCSharp/Form3.Designer.cs
+++ TimeManagerCSharp/Form3.Designer.cs
@@ -69,6 +69,7 @@ namespace TimeManagerCSharp
             this.button1.TabIndex = 2;
             this.button1.Text = "Submit Request";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
--- TimeManagerCSharp/Form3.cs
+++ TimeManagerCSharp/Form3.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
 
 namespace TimeManagerCSharp
 {
@@ -26,5 +28,42 @@ namespace TimeManagerCSharp
             Form1 f1 = new Form1();
             f1.ShowDialog();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //capture text input
+            string body = richTextBox1.Text;
+            System.Windows.Forms.MessageBox.Show(body);
+
+
+            //capture time 
+            DateTime now = DateTime.Now;
+            string nowFormat = now.ToString("yyyy - MM - dd hh: mm:ss");
+            string subject = string.Format("TimeManager Adjustment Request {0}", nowFormat);
+
+            var fromAddress = new MailAddress(", "From Name");
+            var toAddress = new MailAddress("", "To Name");
+            const string fromPassword = "";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
+
+        }    
     }
 }
