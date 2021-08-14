00444f00c6d985cd60c7d03cbf4e6534a0719768
Nicholas Almeida <n.almeida330@gmail.com>
2021-08-14 10:31:00 -0600

    minor design adjustments

--- TimeManagerCSharp/AdminMenu.Designer.cs
+++ TimeManagerCSharp/AdminMenu.Designer.cs
@@ -43,51 +43,57 @@ namespace TimeManagerCSharp
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(85, 99);
+            this.dateTimePicker1.Location = new System.Drawing.Point(74, 74);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(250, 27);
+            this.dateTimePicker1.Size = new System.Drawing.Size(219, 23);
             this.dateTimePicker1.TabIndex = 2;
             // 
             // dateTimePicker2
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(85, 241);
+            this.dateTimePicker2.Location = new System.Drawing.Point(74, 181);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(250, 27);
+            this.dateTimePicker2.Size = new System.Drawing.Size(219, 23);
             this.dateTimePicker2.TabIndex = 3;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.LimeGreen;
-            this.button1.Location = new System.Drawing.Point(85, 317);
+            this.button1.Location = new System.Drawing.Point(74, 238);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(250, 71);
+            this.button1.Size = new System.Drawing.Size(219, 53);
             this.button1.TabIndex = 4;
             this.button1.Text = "Get Payroll Hours";
             this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(459, 99);
+            this.button2.Location = new System.Drawing.Point(402, 74);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(235, 29);
+            this.button2.Size = new System.Drawing.Size(206, 22);
             this.button2.TabIndex = 6;
             this.button2.Text = "Add Employee";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(459, 176);
+            this.button3.Location = new System.Drawing.Point(402, 128);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(235, 29);
+            this.button3.Size = new System.Drawing.Size(206, 22);
             this.button3.TabIndex = 7;
             this.button3.Text = "Remove Employee";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(459, 242);
+            this.button4.Location = new System.Drawing.Point(402, 182);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(235, 29);
+            this.button4.Size = new System.Drawing.Size(206, 22);
             this.button4.TabIndex = 8;
             this.button4.Text = "Adjust Employee Breaks";
             this.button4.UseVisualStyleBackColor = true;
@@ -95,9 +101,10 @@ namespace TimeManagerCSharp
             // button5
             // 
             this.button5.BackColor = System.Drawing.Color.Red;
-            this.button5.Location = new System.Drawing.Point(459, 317);
+            this.button5.Location = new System.Drawing.Point(402, 238);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(235, 71);
+            this.button5.Size = new System.Drawing.Size(206, 53);
             this.button5.TabIndex = 9;
             this.button5.Text = "Return to Employee Area";
             this.button5.UseVisualStyleBackColor = false;
@@ -106,35 +113,35 @@ namespace TimeManagerCSharp
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(147, 41);
+            this.label1.Location = new System.Drawing.Point(129, 31);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(119, 20);
+            this.label1.Size = new System.Drawing.Size(93, 15);
             this.label1.TabIndex = 10;
             this.label1.Text = "Payroll Star Date";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(147, 176);
+            this.label2.Location = new System.Drawing.Point(129, 132);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(118, 20);
+            this.label2.Size = new System.Drawing.Size(93, 15);
             this.label2.TabIndex = 11;
             this.label2.Text = "Payroll End Date";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(550, 41);
+            this.label3.Location = new System.Drawing.Point(481, 31);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(61, 20);
+            this.label3.Size = new System.Drawing.Size(49, 15);
             this.label3.TabIndex = 12;
             this.label3.Text = "Options";
             // 
             // AdminMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 451);
+            this.ClientSize = new System.Drawing.Size(700, 338);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -145,6 +152,7 @@ namespace TimeManagerCSharp
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dateTimePicker2);
             this.Controls.Add(this.dateTimePicker1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "AdminMenu";
             this.Text = "Time Manager - Admin Area";
             this.ResumeLayout(false);
