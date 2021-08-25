17a3e38b647d5105b3fcfcdabc794436fde21046
Nicholas Almeida <n.almeida330@gmail.com>
2021-08-24 22:13:35 -0600

    added sql connection test variable

--- TimeManagerCSharp/MainMenu.cs
+++ TimeManagerCSharp/MainMenu.cs
@@ -35,8 +35,8 @@ namespace TimeManagerCSharp
             pictureBox1.Image = Properties.Resources.nslogo;
             // clear and connect to database
             MySqlConnection.ClearAllPools();
-            MySqlConnection conn = new MySqlConnection("server = localhost; user id = backslash330; password = UrsaMinor; persistsecurityinfo = True; database = timemanager");
-
+            MySqlConnection conn2 = new MySqlConnection("server = localhost; user id = backslash330; password = UrsaMinor; persistsecurityinfo = True; database = timemanager");
+            MySqlConnection conn = new MySqlConnection("server = 192.168.56.1; user id = backslash330; password = UrsaMinor; persistsecurityinfo = True; database = timemanager");
 
             // populate listbox with active employee names from employees table            
             try
